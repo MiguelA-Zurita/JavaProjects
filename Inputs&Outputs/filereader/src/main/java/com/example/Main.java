ba221793125a5b875ea81bf26a0964d0279330ee
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-05-09 18:06:47 +0200

    Read functionality added

--- Inputs&Outputs/filereader/src/main/java/com/example/Main.java
+++ Inputs&Outputs/filereader/src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.*;
+import java.nio.*;
+import java.nio.file.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -26,10 +28,11 @@ public class Main {
         JButton leer = new JButton("Leer");
         leer.addActionListener(listenerUtils.leerListener(path, contenido));
         JButton escribir = new JButton("Escribir");
-        JCheckBox sobreEscribir = new JCheckBox("Añadir");
+        JCheckBox afegir = new JCheckBox("Añadir");
+        escribir.addActionListener(listenerUtils.escribirListener(path, contenido, afegir));
         opciones.add(leer);
         opciones.add(escribir);
-        opciones.add(sobreEscribir);
+        opciones.add(afegir);
         
 
 
@@ -48,21 +51,19 @@ class listenerUtils{
         ActionListener listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String path = inputUser.getText().trim();
+                String contenido = "";
+                int caracter = 0;
                 try {
-                    String path = inputUser.getText().trim();
-                    String contenido = "";
-                    int caracter = 0;
                     FileInputStream inStr = new FileInputStream(path);
-                    System.out.println(inStr.available());
-                    BufferedInputStream buffer = new BufferedInputStream(inStr);
                     while ((caracter = inStr.read()) != -1) {
                         contenido += (char)caracter;
                     }
                     display.setText(contenido);
-                    buffer.close();
+                    inStr.close();
+                    JOptionPane.showMessageDialog(display, "Se ha leído correctamente!", "Mensaje", 1);
                 } catch (Exception ex) {
-                    ex.printStackTrace();
-                    display.setText("Ha ocurrido un error!");
+                    JOptionPane.showMessageDialog(null, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0);
                 }
             }
         };
@@ -71,13 +72,38 @@ class listenerUtils{
 
     }
     //TODO: Acabar esto
-/*     static ActionListener escribirListener (JTextArea display){
+    static ActionListener escribirListener (JTextField inputUser, JTextArea display, JCheckBox afegir){
         ActionListener listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String path = inputUser.getText().trim();
+                byte[] arraySerializado = new byte[display.getText().length()];
+                char[] arrayNoSerializado = display.getText().toCharArray();
+
+                try{
+                    for(int i = 0; i<display.getText().length(); i++){
+                        arraySerializado[i] = (byte) arrayNoSerializado[i];
+                    }
+                    FileOutputStream outStr = new FileOutputStream(path, afegir.isSelected());
+                    
+                    outStr.write(arraySerializado);
+                    outStr.close();
+                    JOptionPane.showMessageDialog(display, "Se ha escrito correctamente!", "Mensaje", 1);
+                } catch(Exception ex){
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0);
+                }
                 
             }
         };
         return listener;
+    }
+
+/*     static void write(final String s) throws IOException {
+        Files.writeString(
+            Path.of(System.getProperty("java.io.tmpdir"), "filename.txt"),
+            s + System.lineSeparator(),
+            StandardOpenOption.CREATE, StandardOpenOption.APPEND
+        );
     } */
 }
