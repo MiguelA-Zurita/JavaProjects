e4fb2f39e7188ef6ce4fdccec08dfbb1b37a1cae
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-05-10 19:39:55 +0200

    Commented

--- Inputs&Outputs/filereader/src/main/java/com/example/Main.java
+++ Inputs&Outputs/filereader/src/main/java/com/example/Main.java
@@ -1,3 +1,6 @@
+
+
+
 package com.example;
 
 import java.io.*;
@@ -7,93 +10,91 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("File reader and Writer");
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.setSize(500,500);
-        
-        JPanel buscador = new JPanel();
-        buscador.setLayout(new FlowLayout());
-        buscador.add(new JLabel("Nombre del fichero:"));
-        JTextField path = new JTextField(30);
-        buscador.add(path);
-        
-        JTextArea contenido = new JTextArea();
-        contenido.setLineWrap(true);
-        contenido.setMargin(new Insets(5, 5, 5, 5));
+        JFrame mainFrame = new JFrame("File reader and Writer"); //Creamos el JFrame
+        mainFrame.setLayout(new BorderLayout()); //Definimos el layout
+        mainFrame.setSize(500, 500); //Definimos el tamaño
 
-        JPanel opciones = new JPanel();
-        opciones.setLayout(new FlowLayout());
-        JButton leer = new JButton("Leer");
-        leer.addActionListener(listenerUtils.leerListener(path, contenido));
-        JButton escribir = new JButton("Escribir");
-        JCheckBox afegir = new JCheckBox("Añadir");
-        escribir.addActionListener(listenerUtils.escribirListener(path, contenido, afegir));
-        opciones.add(leer);
-        opciones.add(escribir);
-        opciones.add(afegir);
+        JPanel buscador = new JPanel(); //Creamos el JPanel
+        buscador.setLayout(new FlowLayout()); //Definimos el layout
+        buscador.add(new JLabel("Nombre del fichero:")); //Añadimos un JLabel para la ruta del fichero
+        JTextField path = new JTextField(30); //Creamos un JTextField para la ruta del fichero
+        buscador.add(path); //Añadimos el JTextField al JPanel
+
+        JTextArea contenido = new JTextArea(); //Creamos un JTextArea para mostrar el contenido del fichero
+        contenido.setLineWrap(true); //Definimos el ajuste de línea
+        contenido.setMargin(new Insets(5, 5, 5, 5)); //Definimos el margen
+
+        JPanel opciones = new JPanel(); //Creamos un JPanel para los botones
+        opciones.setLayout(new FlowLayout()); //Definimos el layout
         
-        mainFrame.add(buscador, BorderLayout.NORTH);
-        mainFrame.add(opciones, BorderLayout.SOUTH);
-        mainFrame.add(contenido, BorderLayout.CENTER);
-        mainFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setVisible(true);
+        JButton leer = new JButton("Leer"); //Creamos un JButton para leer el fichero
+        leer.addActionListener(listenerUtils.leerListener(path, contenido)); //Añadimos el ActionListener al botón de lectura
+        JButton escribir = new JButton("Escribir"); //Creamos un JButton para escribir en el fichero
+        JCheckBox afegir = new JCheckBox("Añadir"); //Creamos un JCheckBox en caso de que se quiera añadir texto al fichero
+        escribir.addActionListener(listenerUtils.escribirListener(path, contenido, afegir)); //Añadimos el ActionListener al botón de escritura
+        opciones.add(leer); //Añadimos el botón de lectura al panel de opciones
+        opciones.add(escribir); //Añadimos el botón de escritura al Panel de opciones
+        opciones.add(afegir); //Añadimos el JCheckBox al Panel de opciones
+
+        mainFrame.add(buscador, BorderLayout.NORTH); //Añadimos el JPanel de búsqueda al JFrame
+        mainFrame.add(opciones, BorderLayout.SOUTH); //Añadimos el JPanel de opciones al JFrame
+        mainFrame.add(contenido, BorderLayout.CENTER); //Añadimos el JTextArea al JFrame
+        mainFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE); //Definimos la operación de cierre
+        mainFrame.setLocationRelativeTo(null); //Centramos la ventana
+        mainFrame.setVisible(true); //Hacemos visible el JFrame
     }
 }
 
-class listenerUtils{
-    
-    static ActionListener leerListener(JTextField inputUser, JTextArea display){
+class listenerUtils {
+
+    static ActionListener leerListener(JTextField inputUser, JTextArea display) { //Método para crear el ActionListener para leer el fichero
         ActionListener listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String path = inputUser.getText().trim();
-                String contenido = "";
-                int caracter = 0;
+                String path = inputUser.getText().trim(); //Obtenemos la ruta del fichero
+                String contenido = ""; //Creamos una variable para almacenar el contenido del fichero
+                int caracter = 0; //Creamos una variable para almacenar el caracter leído cuando deserializamos el fichero
                 try {
-                    FileInputStream inStr = new FileInputStream(path);
-                    while ((caracter = inStr.read()) != -1) {
-                        contenido += (char)caracter;
+                    FileInputStream inStr = new FileInputStream(path); //Creamos un FileInputStream para leer el fichero
+                    while ((caracter = inStr.read()) != -1) { //Leemos el fichero hasta el final
+                        contenido += (char) caracter; //Convertimos el byte leído a char y lo añadimos al contenido
                     }
-                    display.setText(contenido);
-                    inStr.close();
-                    JOptionPane.showMessageDialog(display, "Se ha leído correctamente!", "Mensaje", 1);
+                    display.setText(contenido); //Mostramos el contenido del fichero en el JTextArea
+                    inStr.close(); //Cerramos el FileInputStream
+                    JOptionPane.showMessageDialog(display, "Se ha leído correctamente!", "Mensaje", 1); //Mostramos un dialogo de éxito
                 } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(display, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0);
+                    JOptionPane.showMessageDialog(display, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0); //Mostramos un dialogo de error en caso de que no se encuentre el fichero
                 }
             }
         };
-        return listener;
+        return listener; //Devolvemos el ActionListener
     }
 
-    static ActionListener escribirListener (JTextField inputUser, JTextArea display, JCheckBox afegir){
+    static ActionListener escribirListener(JTextField inputUser, JTextArea display, JCheckBox afegir) { //Método para crear el ActionListener para escribir en el fichero
         ActionListener listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String path = inputUser.getText().trim();
-                byte[] arraySerializado = new byte[display.getText().length()+(afegir.isSelected() ? System.lineSeparator().length() : 0)];
-                char[] arrayNoSerializado = display.getText().toCharArray();
-                try{
-                    if(afegir.isSelected()){
-                        for(int i = 0; i<System.lineSeparator().length(); i++){
-                            arraySerializado[i] = (byte) System.lineSeparator().charAt(i);
+                String path = inputUser.getText().trim(); //Obtenemos la ruta del fichero
+                byte[] arraySerializado = new byte[display.getText().length() + (afegir.isSelected() ? System.lineSeparator().length() : 0)]; //Creamos un array de bytes para almacenar el contenido que se va a escribir en el fichero 
+                char[] arrayNoSerializado = display.getText().toCharArray(); //Convertimos el contenido del JTextArea a un array de chars para serializarlo posteriormente
+                try {
+                    if (afegir.isSelected()) { //Si se ha seleccionado el JCheckBox de añadir, añadimos el salto de línea al array serializado
+                        for (int i = 0; i < System.lineSeparator().length(); i++) { //Recorremos el array de bytes
+                            arraySerializado[i] = (byte) System.lineSeparator().charAt(i); //Convertimos el salto de línea a byte y lo añadimos al array serializado
                         }
-                        System.out.println("Ha pasado la linea separativa");
                     }
-                    for(int i = 0; i<arrayNoSerializado.length; i++){
-                        arraySerializado[i+(afegir.isSelected() ? System.lineSeparator().length() : 0)] = (byte) arrayNoSerializado[i];
+                    for (int i = 0; i < arrayNoSerializado.length; i++) { //Recorremos el array de chars
+                        arraySerializado[i + (afegir.isSelected() ? System.lineSeparator().length(): 0)] = (byte) arrayNoSerializado[i]; //Convertimos el char a byte y lo añadimos al array serializado
                     }
-                    FileOutputStream outStr = new FileOutputStream(path, afegir.isSelected());
-                    outStr.write(arraySerializado);
-                    outStr.close();
-                    JOptionPane.showMessageDialog(display, "Se ha escrito correctamente!", "Mensaje", 1);
-                } catch(Exception ex){
-                    ex.printStackTrace();
-                    JOptionPane.showMessageDialog(display, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0);
+                    FileOutputStream outStr = new FileOutputStream(path, afegir.isSelected()); //Creamos un FileOutputStream para escribir en el fichero, en caso de que se haya seleccionado el JCheckBox de añadir, se añade el contenido al final del fichero
+                    outStr.write(arraySerializado); //Escribimos el array serializado en el fichero
+                    outStr.close(); //Cerramos el FileOutputStream
+                    JOptionPane.showMessageDialog(display, "Se ha escrito correctamente!", "Mensaje", 1); //Mostramos un dialogo de éxito
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(display, "El archivo no se ha encontrado!", "FILE_NOT_FOUND", 0); //Mostramos un dialogo de error en caso de que no se encuentre el fichero
                 }
-                
             }
         };
-        return listener;
+        return listener; //Devolvemos el ActionListener
     }
 }
